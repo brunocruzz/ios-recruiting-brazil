6e511b2537d1012ccafc13c8105fd4caf98bf877
brunoCruz <brunohmcruz@icloud.com>
2019-11-24 20:44:19 -0300

    Adding Nimble_Snapshots

--- ConcreteApplicationApp/ConcreteApplicationTests/Controllers/MoviesGridViewControllerSpec.swift
+++ ConcreteApplicationApp/ConcreteApplicationTests/Controllers/MoviesGridViewControllerSpec.swift
@@ -8,6 +8,7 @@
 
 import Quick
 import Nimble
+import Nimble_Snapshots
 
 @testable import ConcreteApplication
 
@@ -15,22 +16,35 @@ class MoviesGridViewControllerSpec: QuickSpec {
     
     override func spec() {
         
+        var window: UIWindow!
         var sut: MoviesGridViewController!
         
-        beforeEach {
-            sut = MoviesGridViewController()
-            _ = sut.view
-        }
-        
-        afterEach {
-            sut = nil
-        }
-        
-        context("when initializang controller") {
-            it("should not be nil") {
-                expect(sut).toNot(beNil())
+        describe("MoviesGridViewController") {
+            
+            beforeEach {
+                
+                sut = MoviesGridViewController()
+                
+                window = UIWindow(frame: UIScreen.main.bounds)
+                window.rootViewController = sut
+                window.makeKeyAndVisible()
+                _ = sut.view
+            }
+            
+            afterEach {
+                sut = nil
+            }
+            
+            context("when initialized") {
+                
+                it("should not be nil") {
+                    expect(sut).toNot(beNil())
+                }
+                
+                it("should have tge expected layout for first state") {
+                    expect(window) == snapshot("MoviesGridViewController_empty_list")
+                }
             }
         }
-        
     }
 }
